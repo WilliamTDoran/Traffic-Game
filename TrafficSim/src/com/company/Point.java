27c0054075b95549787d77aff93b6dfaef4464f9
acapple <acapple2002@gmail.com>
2024-02-26 16:23:29 -0500

    Filled point & pos

--- TrafficSim/src/com/company/Point.java
+++ TrafficSim/src/com/company/Point.java
@@ -3,4 +3,13 @@ package com.company;
 public class Point {
     double x;
     double y;
+
+    public double X() { return x; }
+
+    public double Y() { return y; }
+
+    public Point(double X, double Y) {
+        x = X;
+        y = Y;
+    }
 }
--- TrafficSim/src/com/company/Position.java
+++ TrafficSim/src/com/company/Position.java
@@ -3,4 +3,21 @@ package com.company;
 public class Position {
     TrafficElement trafficElement;
     Point coordinate;
+
+    public TrafficElement getTrafficElement() {
+        return trafficElement;
+    }
+
+    public Point getPosition() {
+        return coordinate;
+    }
+
+    public Position(TrafficElement element, int x, int y) {
+        coordinate = new Point(x, y);
+        trafficElement = element;
+    }
+    public Position(TrafficElement element, Point position) {
+        coordinate = position;
+        trafficElement = element;
+    }
 }
