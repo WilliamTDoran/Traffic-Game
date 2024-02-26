6161696d485cd5ed0453a0ed8ebf13578e4a0bb9
acapple <acapple2002@gmail.com>
2024-02-26 16:10:27 -0500

    Added player & filled GE & MoveCntrl

--- TrafficSim/src/com/company/GameEngine.java
+++ TrafficSim/src/com/company/GameEngine.java
@@ -1,4 +1,37 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class GameEngine implements MovementControl {
+    TrafficNetwork trafficNetwork;
+    ArrayList<Vehicle> vehicles;
+    Player player;
+    MovementControl movementControl;
+    ChallengeHandle challengeHandle;
+    int turnCount;
+
+
+    public ArrayList<Vehicle> updateVehiclesPosition(TrafficNetwork network, ArrayList<Vehicle> vehicles) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> checkRegion(Vehicle vehicle, ArrayList<Vehicle> vehicles) {
+        return null;
+    }
+
+    public Position validateMoveChoice(Vehicle vehicle, Position newPosition) {
+        return newPosition;
+    }
+
+    public ArrayList<TrafficElement> probeMapSurroundings(Vehicle vehicle) {
+        return null;
+    }
+
+    private void updateSimulationTurn() {
+
+    }
+
+    private void promptPlayer() {
+
+    }
 }
--- TrafficSim/src/com/company/MovementControl.java
+++ TrafficSim/src/com/company/MovementControl.java
@@ -1,4 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public interface MovementControl {
+    public ArrayList<Vehicle> updateVehiclesPosition(TrafficNetwork network, ArrayList<Vehicle> vehicles);
+
+    public ArrayList<Vehicle> checkRegion(Vehicle vehicle, ArrayList<Vehicle> vehicles);
+
+    public Position validateMoveChoice(Vehicle vehicle, Position newPosition);
+
+    public ArrayList<TrafficElement> probeMapSurroundings(Vehicle vehicle);
 }
