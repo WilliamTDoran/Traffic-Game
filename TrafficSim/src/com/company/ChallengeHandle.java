e3c9bb40bad33bdb54e510e6c57a48d125dfc774
acapple <acapple2002@gmail.com>
2024-02-26 16:12:50 -0500

    Added player & filled GE & MoveCntrl

--- TrafficSim/src/com/company/ChallengeHandle.java
+++ TrafficSim/src/com/company/ChallengeHandle.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class ChallengeHandle {
+    private Vehicle findNeighbours(Vehicle vehicle) {
+        return vehicle;
+    }
+
+    public TrafficNetwork runChallenge(Vehicle vehicle, TrafficNetwork trafficNetwork) {
+        return trafficNetwork;
+    }
 }
