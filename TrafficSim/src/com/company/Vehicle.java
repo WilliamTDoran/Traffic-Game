81f9c584139d6d7cab1427d394ac51fa6c5bcbd3
WilliamTDoran <71032493+WilliamTDoran@users.noreply.github.com>
2024-02-26 15:10:51 -0500

    starter

---
+++ TrafficSim/src/com/company/Vehicle.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public abstract class Vehicle
+{
+    private MovementStatus movementStatus;
+    private String color;
+    private Double size;
+    private Double weight;
+    private Double maxSpeed;
+    private Reputation reputation;
+    private DamageStatus damageStatus;
+}
